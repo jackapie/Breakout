86f72c07de4884c4ac187f51ecef1d1d516ace76
Jackie Pearce <jackiepearce83@hotmail.com>
2018-08-06 14:50:29 +0100

    Created classes

--- Breakout/Brick.cs
+++ Breakout/Brick.cs
@@ -6,7 +6,8 @@ using System.Threading.Tasks;
 
 namespace Breakout
 {
-    public class Class1
+    public class Brick : ScreenObject
     {
+        public string Colour { get; set; }
     }
 }
---
+++ Breakout/ScreenObject.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Breakout
+{
+    public class ScreenObject
+
+    {
+        public int X { get; protected set; }
+        public int Y { get; protected set; }
+        public void Move(int dx, int dy)
+        {
+
+        }
+
+    }
+}
